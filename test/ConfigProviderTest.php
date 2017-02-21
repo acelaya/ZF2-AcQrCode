2d5693e5b43657019ad90e40fbe30dc5eddcf490
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-02-21 20:02:11 +0100

    Created ConfigProvider and extra zf composer params

---
+++ test/ConfigProviderTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace AcMailerTest;
+
+use Acelaya\QrCode\ConfigProvider;
+use PHPUnit\Framework\TestCase;
+
+class ConfigProviderTest extends TestCase
+{
+    /**
+     * @var ConfigProvider
+     */
+    private $configProvider;
+
+    protected function setUp()
+    {
+        $this->configProvider = new ConfigProvider();
+    }
+
+    public function testInvoke()
+    {
+        $moduleConfig = include __DIR__ . '/../config/module.config.php';
+        $expectedConfig = ['dependencies' => $moduleConfig['service_manager']];
+        $returnedConfig = $this->configProvider->__invoke();
+
+        $this->assertEquals($expectedConfig, $returnedConfig);
+    }
+}
